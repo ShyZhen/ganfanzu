6b6da80ee9290857cf7cf6a02f27efbacd5d1833
zhenhuaixiu <zhenhuaixiu@sznews.com>
2021-03-04 09:31:09 +0800

    优化about页logo的padding值，弥补自定义导航栏高度的兼容问题

--- pages/about/about.vue
+++ pages/about/about.vue
@@ -1,5 +1,5 @@
 <template>
-	<view class="container">
+  <view class="container">
 
     <!--  header  -->
     <view class="header-container">
@@ -15,17 +15,17 @@
     </view>
 
     <view class="about">
-        <view class="header"><span>敬广大干饭组用户：</span></view>
-		<div class="chapter">
-			<view>1. 欢迎您使用干饭组小程序，干饭组小程序旨为广大用户提供每日领取外卖红包的快捷方式。</view>
-			<view>2. 其中第三方外卖平台皆为官方小程序平台，本项目不会未经允许收集您的任何私人信息。</view>
-			<view>3. 首次使用外卖平台小程序领取红包需授权微信登录，领取的红包可在该平台下的任何客户端查询与使用。</view>
-			<view>4. 建议您添加到“我的小程序”中，方便每次领取红包以及一站式下单。</view>
-			<view>5. 干饭组会持续为广大用户提供每日外卖红包、优质商品推荐等内容。</view>
-			<view>6. 最后感谢每位小可爱的支持，不积跬步，无以至千里，愿与君共勉，冲啊，干饭人！</view>
-		</div>
+      <view class="header"><span>敬广大干饭组用户：</span></view>
+      <view class="chapter">
+        <view>1. 欢迎您使用干饭组小程序，干饭组小程序旨为广大用户提供每日领取外卖红包的快捷方式。</view>
+        <view>2. 其中第三方外卖平台皆为官方小程序平台，本项目不会未经允许收集您的任何私人信息。</view>
+        <view>3. 首次使用外卖平台小程序领取红包需授权微信登录，领取的红包可在该平台下的任何客户端查询与使用。</view>
+        <view>4. 建议您添加到“我的小程序”中，方便每次领取红包以及一站式下单。</view>
+        <view>5. 干饭组会持续为广大用户提供每日外卖红包、优质商品推荐等内容。</view>
+        <view>6. 最后感谢每位小可爱的支持，不积跬步，无以至千里，愿与君共勉，冲啊，干饭人！</view>
       </view>
-	</view>
+    </view>
+  </view>
 </template>
 
 <script>
@@ -48,19 +48,19 @@ export default {
 
 <style lang="scss">
 page {
-	background-color: #fff;
+  background-color: #fff;
 }
 .container {
-	padding: 0 32rpx;
-	font-size: 14px;
-	line-height: 24px;
-	position: relative;
+  padding: 0 32rpx;
+  font-size: 14px;
+  line-height: 24px;
+  position: relative;
   .header-container {
     .header {
       text-align: center;
       .logo-img {
         height: 68px;
-        padding-top: 80rpx;
+        padding-top: 120rpx;
         image {
           width: 68px;
           height: 68px;
@@ -76,94 +76,94 @@ page {
     }
   }
 
-	.about {
-		margin-top: 60rpx;
-		.header{
-			margin-bottom: 32rpx;
-			display: inline-block;
-			position: relative;
-			font-size: 32rpx;
-			font-weight: bold;
-			
-			&::after{
-				content: '';
-				position: absolute;
-				bottom: 0;
-				left: 0;
-				right: 0;
-				height: 12rpx;
-				background-color: #fdefec;
-			}
-			span{
-				position: relative;
-				z-index: 2;
-			}
-		}
-		.chapter{
-			view{
-				position: relative;
-				margin-bottom: 20rpx;
-				padding: 24rpx;
-				background-color: #FFFCFC;
-				border: 1px solid #E8E8E8;
-				color: #666;
-				font-size: 28rpx;
-				line-height: 40rpx;
-				border-radius: 4rpx;
-				
-				&::after{
-					content: '';
-					position: absolute;
-					bottom: 0;
-					right: 0;
-					width: 76rpx;
-					height: 68rpx;
-					background: url(../../static/about/remarks.png);
-					background-size: 288rpx 176rpx;
-				}
-				
-				
-				&:nth-child(even){
-					background-color: #FFF;
-				}
-				
-				&:nth-child(1){
-					&::after{
-						background-position: -106rpx -10rpx;
-					}
-				}
-				
-				&:nth-child(2){
-					&::after{
-						background-position: -10rpx -98rpx;
-					}
-				}
-				
-				&:nth-child(3){
-					&::after{
-						background-position: -106rpx -98rpx;
-					}
-				}
-				
-				&:nth-child(4){
-					&::after{
-						background-position: -202rpx -10rpx;
-					}
-				}
-				
-				&:nth-child(5){
-					&::after{
-						background-position: -202rpx -98rpx;
-					}
-				}
-				
-				&:nth-child(6){
-					&::after{
-						background-position: -10rpx -10rpx;
-					}
-				}
-			}
-		}
-	}
+  .about {
+    margin-top: 60rpx;
+    .header{
+      margin-bottom: 32rpx;
+      display: inline-block;
+      position: relative;
+      font-size: 32rpx;
+      font-weight: bold;
+
+      &::after{
+        content: '';
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        height: 12rpx;
+        background-color: #fdefec;
+      }
+      span{
+        position: relative;
+        z-index: 2;
+      }
+    }
+    .chapter{
+      view{
+        position: relative;
+        margin-bottom: 20rpx;
+        padding: 24rpx;
+        background-color: #FFFCFC;
+        border: 1px solid #E8E8E8;
+        color: #666;
+        font-size: 28rpx;
+        line-height: 40rpx;
+        border-radius: 4rpx;
+
+        &::after{
+          content: '';
+          position: absolute;
+          bottom: 0;
+          right: 0;
+          width: 76rpx;
+          height: 68rpx;
+          background: url(../../static/about/remarks.png);
+          background-size: 288rpx 176rpx;
+        }
+
+
+        &:nth-child(even){
+          background-color: #FFF;
+        }
+
+        &:nth-child(1){
+          &::after{
+            background-position: -106rpx -10rpx;
+          }
+        }
+
+        &:nth-child(2){
+          &::after{
+            background-position: -10rpx -98rpx;
+          }
+        }
+
+        &:nth-child(3){
+          &::after{
+            background-position: -106rpx -98rpx;
+          }
+        }
+
+        &:nth-child(4){
+          &::after{
+            background-position: -202rpx -10rpx;
+          }
+        }
+
+        &:nth-child(5){
+          &::after{
+            background-position: -202rpx -98rpx;
+          }
+        }
+
+        &:nth-child(6){
+          &::after{
+            background-position: -10rpx -10rpx;
+          }
+        }
+      }
+    }
+  }
 }
 </style>
