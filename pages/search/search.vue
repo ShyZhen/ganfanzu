b927abf4954c4be7848e612f7252e35526b3f7e3
zhenhuaixiu <zhenhuaixiu@sznews.com>
2021-03-22 18:37:01 +0800

    op code

--- pages/search/search.vue
+++ pages/search/search.vue
@@ -1,10 +1,15 @@
 <template>
-	<view>
-		<view class="search">
-			<view class="search-input">
-				<image class="search-icon" src="../../static/icon/search.png" mode="widthFix" lazy-load @load="onoff='1'"></image>
-				<input type="text" placeholder="搜索商品关键字..." maxlength="32" confirm-type="search" v-model="query" @confirm="searchList">
+	<view class="container" :style="{opacity:pageOpacity}">
+		<view class="header" :style="{paddingTop: searchInput.top + 'px'}">
+			<view class="search">
+				<view class="search-input" :style="{width:searchInput.width+'px', height:searchInput.height+'px'}">
+					<image class="search-icon" src="../../static/icon/search.png" mode="widthFix" lazy-load @load="onoff='1'"></image>
+					<input type="text" placeholder="搜索商品关键字..." maxlength="32" confirm-type="search" v-model="query" @confirm="searchList">
+				</view>
 			</view>
+		</view>
+
+		<view class="coupon" :style="{marginTop: headerHeight + 'px'}">
 		</view>
 	</view>
 </template>
@@ -16,7 +21,27 @@
 			return {
 				query: '',
 				platform: 'jd',
+				/**
+				 * 状态栏高度、搜索框位置（胶囊对其）
+				 **/
+				headerHeight: 0,
+				searchInput: {
+					width: 0,
+					height: 0,
+					top: 0,
+					inputVal: ''
+				},
 			}
+		},
+		onLoad() {
+			// 胶囊宽高坐标
+			this.searchInput.width = (this.$systemInfoSync.windowWidth - this.$menuButtonRect.width) - 24
+			this.searchInput.height = this.$menuButtonRect.height || 32
+			this.searchInput.top = this.$menuButtonRect.top || 12
+			this.headerHeight = this.searchInput.top + this.searchInput.height + 36 + 12;
+		},
+		onReady() {
+			this.pageOpacity = 1
 		},
 		methods: {
 			/**
@@ -65,22 +90,40 @@
 </script>
 
 <style lang="scss">
+page {
+	background-color: #fff;
+}
+
+.container {
+	transition: all 0.5s linear;
+	font-size: 14px;
+	line-height: 24px;
+	position: relative;
+}
+
+.header {
+	box-sizing: border-box;
+}
+
 /*搜索框*/
 .search {
 	display: flex;
 	position: fixed;
 	width: 100%;
-	
-	&-input {
+	padding: 0 24rpx;
+	box-sizing: border-box;
+
+	.search-input {
 		height: 100%;
 		border-radius: 20px;
-		background: #ffffffd6;
+		background: #F2F2F2;
 		color: rgba(68, 66, 66, 0.63);
 		display: flex;
 		/* justify-content: center; */
 		align-items: center;
+
 		input {
-			padding-left: 30rpx;
+			padding-left: 18rpx;
 			width: 75%;
 		}
 		image {
--- pages/shop/shop.vue
+++ pages/shop/shop.vue
@@ -15,11 +15,11 @@
 				<v-fake-search :height="searchInput.height" :width="searchInput.width"></v-fake-search>
 			</view>
 			<view class="tabs">
-				<v-tabs 
-					v-model="current" 
-					:tabs="tabs" 
-					@change="changeTab" 
-					class="tab" 
+				<v-tabs
+					v-model="current"
+					:tabs="tabs"
+					@change="changeTab"
+					class="tab"
 					bgColor="#F46845"
 					color="rgba(255,255, 255, .65)"
 					activeColor="#fff"
@@ -27,7 +27,7 @@
 				></v-tabs>
 			</view>
 		</view>
-		
+
 		<!--  内容  -->
 		<view class="coupon" ref="coupon" :style="{marginTop: headerHeight + 'px'}">
 			<view v-for="(tab, i) in tabs" v-show="tab.type === tabType" :key="i">
@@ -49,7 +49,7 @@
 				<v-loading v-show="!couponListInfo[tab.type].canLoadMore" :type="1" ></v-loading>
 			</view>
 		</view>
-		
+
 		<v-loading v-show="isLoading"></v-loading>
 	</view>
 </template>
@@ -66,10 +66,12 @@
 		getDetailLink,
 		getCustomProduct,
 	} from "@/apis/ganfan.js"
+	import VFakeSearch from "../../components/v-fake-search/v-fake-search";
 
 	// 默认选中tab 下饭必备
 	const DEFAULR_CHECKED_TYPE = 'custom'
 	export default {
+		components: {VFakeSearch},
 		data() {
 			return {
 				isLoading: false,
@@ -88,7 +90,6 @@
 				 * 状态栏高度、搜索框位置（胶囊对其）
 				 **/
 				headerHeight: 0,
-				statusBarHeight: 0,
 				searchInput: {
 					width: 0,
 					height: 0,
@@ -114,7 +115,6 @@
 			// 系统屏幕宽高、状态栏高度
 			//this.height = this.$systemInfoSync.windowHeight
 			//this.width = this.$systemInfoSync.windowWidth
-			this.statusBarHeight = this.$systemInfoSync.statusBarHeightx
 			console.log(this.$systemInfoSync.windowWidth, this.$menuButtonRect.width)
 			// 胶囊宽高坐标
 			this.searchInput.width = (this.$systemInfoSync.windowWidth - this.$menuButtonRect.width) - 24
@@ -133,7 +133,7 @@
 			let {canLoadMore, page} = couponListInfo;
 			// 没有更多加载
 			if(!canLoadMore) return;
-			
+
 			switch(type) {
 				case DEFAULR_CHECKED_TYPE:
 					break;
@@ -210,10 +210,10 @@
 						this.$toast(res.msg || '您的网络状态不太好哦~')
 						return
 					}
-					const 
+					const
 						couponListInfo = this.couponListInfo[platform],
 						handleList = this.handleData(res.data);
-						
+
 					this.setChoosedTab(platform);
 					Object.assign(couponListInfo, {
 						page,
@@ -418,7 +418,7 @@
 		font-size: 14px;
 		line-height: 24px;
 		position: relative;
-		
+
 		.tab {
 			// //position: fixed;
 			// top: var(--window-top);
