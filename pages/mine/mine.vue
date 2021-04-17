38aee8dcf621ee1ec8483a73024b587290dc4f8a
zhenhuaixiu <huaixiu.zhen@gmail.com>
2021-04-17 15:16:05 +0800

    login && mine

--- pages/mine/mine.vue
+++ pages/mine/mine.vue
@@ -1,232 +1,249 @@
 <template>
-  <view class="t-login">
-    <view class="t-b">{{title}}</view>
-    <form class="cl">
+	<view class="container" :style="{opacity:pageOpacity}">
+		<view class="head comm-center">
+			<image :src="user.avatar ? user.avatar : '/static/default_avatar.jpg'"></image>
+			<view class="name">{{user.name}}</view>
+			<view class="remarks">{{user.bio ? user.bio : '说点啥好呢~'}}</view>
+		</view>
+		<view class="home-menu">
+			<view class="row comm-center">
+				<view>{{user.id}}</view>
+				<view>我的ID</view>
+			</view>
+			<view class="row edge comm-center">
+				<view>23</view>
+				<view>我的日记</view>
+			</view>
+			<view class="row comm-center">
+				<view>45</view>
+				<view>我的评论</view>
+			</view>
+		</view>
+		<view class="list-menu">
+			<view class="cell">
+				<view class="left-icon comm-center">
+					<image src="/static/icon/xiaomai_2.png"></image>
+				</view>
+				<view class="txt">我的鼻血</view>
+				<view class="right-icon comm-center">
+					<image src="/static/icon/youjiantou.png"></image>
+				</view>
+			</view>
+			<view class="cell">
+				<view class="left-icon comm-center">
+					<image src="/static/icon/xiaomai_2.png"></image>
+				</view>
+				<view class="txt">我的眼泪</view>
+				<view class="right-icon comm-center">
+					<image src="/static/icon/youjiantou.png"></image>
+				</view>
+			</view>
+			<view class="cell">
+				<view class="left-icon comm-center">
+					<image src="/static/icon/xiaomai_2.png"></image>
+				</view>
+				<view class="txt">我的便便</view>
+				<view class="right-icon comm-center">
+					<image src="/static/icon/youjiantou.png"></image>
+				</view>
+			</view>
+			<view class="cell">
+				<view class="left-icon comm-center">
+					<image src="/static/icon/xiaomai_2.png"></image>
+				</view>
+				<view class="txt">我的帅气</view>
+				<view class="right-icon comm-center">
+					<image src="/static/icon/youjiantou.png"></image>
+				</view>
+			</view>
+		</view>
 
-      <view class="t-a">
-        <image src="/static/icon/mobile.png"></image>
-        <input type="text" clearable v-model="account" @input="checkIsCorAccount" placeholder="请输入手机号" />
-      </view>
-
-      <view class="t-a">
-        <image src="/static/icon/code.png"></image>
-        <input type="text" clearable v-model="verify_code" placeholder="请输入验证码" />
-        <view class="t-c" @tap="registerCode">{{codeDuration ? codeDuration + 's' : '发送短信' }}</view>
-      </view>
-
-      <view class="t-d">未注册的手机号验证后将自动注册</view>
-
-      <view>
-        <CcButton @cctap="showLoading('loginLoading')" width="500rpx" color="#fff" bgcolor="linear-gradient(-45deg, rgba(246, 112, 79, 1) 0%, rgba(243, 49, 35, 1) 100%);"
-                  :loading="loginLoading" @tap="login">立即登录</CcButton>
-      </view>
-
-    </form>
-    <view class="t-f">登录即同意<text>用户协议</text></view>
-    <view class="t-e cl">
-      <view class="t-g">
-        <image src="/static/icon/weixin.png"></image>
-        <view>微信</view>
-      </view>
-    </view>
-  </view>
+		<view class="logout">
+			<CcButton @cctap="showLoading('logoutLoading')" width="500rpx" color="#fff" bgcolor="linear-gradient(-45deg, rgba(246, 112, 79, 1) 0%, rgba(243, 49, 35, 1) 100%);"
+					  :loading="logoutLoading" @tap="logout">退出登录</CcButton>
+		</view>
+	</view>
 </template>
+
 <script>
-import CcButton from '@/components/cc-button/cc-button.vue'
-import { mapState, mapActions } from 'vuex'
-import * as Common from '@/utils/common.js'
-import { registerCode, loginQuick } from '@/utils/loginPlugin.js'
+	import CcButton from '@/components/cc-button/cc-button.vue'
+	import { mapState, mapActions } from 'vuex'
+	import { getMyInfo } from '@/apis/users.js'
+	import { logout } from '@/utils/loginPlugin.js'
 
-export default {
-  data() {
-    return {
-      title: '欢迎回来',
-      loginLoading: false,
-      account: '',
-      // 是否是正确的邮箱或者手机号码
-      isCorrectAccount: false,
-      // 是否可以获取注册码|剩余时间
-      codeDuration: 0,
-      // 验证码
-      verify_code: '',
-    }
-  },
-  computed: {
-    ...mapState(['hasBinding', 'hasLogin']),
-  },
-  components:{
-    CcButton
-  },
-  onLoad() {
-    // 在需要登录的地方执行初始化方法
-    this.initLoginState()
+	export default {
+		data() {
+			return {
+				pageOpacity: 0,
+				user: {},
+				logoutLoading: false,
+			}
+		},
+		components:{
+			CcButton
+		},
+		onLoad: function () {
 
-    // 判断登录状态 并跳转到首页
-    if (this.hasLogin) {
-      this.$toHome()
-    }
-  },
-  methods: {
-    ...mapActions(['initLoginState']),
+			// 在需要登录的地方执行初始化方法
+			this.initLoginState()
 
-    checkIsCorAccount() {
-      const account = this.account;
-      this.isCorrectAccount = Common.regular('phone', account)
-    },
+			// 判断登录状态 并跳转到首页
+			if (!this.hasLogin) {
+				this.$toLogin()
+			} else {
+				this.initUserInfo()
+			}
+		},
+		onReady(e) {
+			this.pageOpacity = 1
+		},
+		computed: {
+			...mapState(['hasBinding', 'hasLogin']),
+		},
+		methods: {
+			...mapActions(['initLoginState']),
 
-    registerCode() {
-      if (this.codeDuration) {
-        this.$toast(`请在${this.codeDuration}秒后重试`)
-        return
-      }
-      if (!this.isCorrectAccount) {
-        this.$toast('手机号码格式不对/(ㄒoㄒ)/~~')
-        return
-      }
+			initUserInfo() {
+				this.$loading()
+				getMyInfo().then(res => {
+					this.user = res.data
+					this.$loading(false)
+				}).catch(err => {
+					this.$loading(false)
+				})
+			},
 
-      // TODO 极验
-
-      let data = {
-        account: this.account
-      }
-      registerCode(data).then(res => {
-        this.$toast(res.message)
-        // 触发倒计时
-        this.codeDuration = 60
-        this.codeInterVal = setInterval(() => {
-          this.codeDuration--
-          if (this.codeDuration === 0) {
-            if (this.codeInterVal) {
-              clearInterval(this.codeInterVal)
-              this.codeInterVal = null
-            }
-          }
-        }, 1000)
-      }).catch(err => {
-        this.$toast('验证码发送失败：' + err.data.message)
-      })
-    },
-
-    login() {
-      let that = this
-
-      loginQuick(this.account, this.verify_code).then(res => {
-        that.$toast('欢迎回来')
-        setTimeout(() => {
-          that.saveLoading = false
-          that.$toHome()
-        }, 500);
-      })
-    },
-
-    showLoading(type, ttl = 3500) {
-      this[type] = true
-      setTimeout(() => {
-        this[type] = false
-      }, ttl);
-    },
-  }
-}
+			logout() {
+				this.bindLogout()
+			},
+			bindLogout() {
+				let that = this
+				logout().then(res => {
+					setTimeout(() => {
+						that.logoutLoading = false
+						that.$toLogin()
+					}, 500);
+				})
+			},
+			showLoading(type, ttl = 3500) {
+				this[type] = true
+				setTimeout(() => {
+					this[type] = false
+				}, ttl);
+			},
+		}
+	}
 </script>
-<style>
-.t-login {
-  width: 600rpx;
-  margin: 0 auto;
-  font-size: 28rpx;
-  color: #000
-}
 
-.t-login input {
-  padding: 0 20rpx 0 120rpx;
-  height: 90rpx;
-  line-height: 90rpx;
-  margin-bottom: 50rpx;
-  background: #f4f4f4;
-  font-size: 28rpx;
-  border-radius: 50rpx
-}
+<style lang="scss">
+	page {
+		background-color: #f8f8f8;
+	}
+	.container {
+		transition: all 0.5s linear;
+		font-size: 14px;
+		line-height: 24px;
+		position: relative;
+	}
+	.comm-center {
+		display: -webkit-box;
+		display: -webkit-flex;
+		display: flex;
+		-webkit-box-pack: center;
+		-webkit-justify-content: center;
+		justify-content: center;
+		-webkit-box-align: center;
+		-webkit-align-items: center;
+		align-items: center;
+		text-align: center;
+	}
 
-.t-login .t-a {
-  position: relative
-}
+	.head {
+		padding-top: 10px;
+		height: 400rpx;
+		flex-direction: column;
+		background: linear-gradient(#F46845, #fa856b);
+		color: #fbf1ef;
 
-.t-login .t-a image {
-  width: 40rpx;
-  height: 40rpx;
-  position: absolute;
-  left: 40rpx;
-  top: 28rpx;
-  border-right: 2rpx solid #dedede;
-  padding-right: 20rpx
-}
+		image {
+			width: 120rpx;
+			height: 120rpx;
+			border: 5rpx solid #FFFFFF;
+			border-radius: 100%;
+		}
 
-.t-login .t-b {
-  text-align: center;
-  font-size: 46rpx;
-  color: #000;
-  padding: 150rpx 0
-}
+		.name {
+			padding-top: 30rpx;
+			font-size: 35rpx;
+		}
 
-.t-login .t-c {
-  position: absolute;
-  right: 22rpx;
-  top: 22rpx;
-  background: linear-gradient(-45deg, rgba(246, 112, 79, 1) 0%, rgba(243, 49, 35, 1) 100%);
-  color: #fff;
-  font-size: 10px;
-  border-radius: 50rpx;
-  height: 50rpx;
-  line-height: 50rpx;
-  padding: 0 25rpx;
-  z-index: 2;
-}
+		.remarks {
+			padding-top: 10rpx;
+			font-size: 24rpx;
+		}
+	}
 
-.t-login .t-d {
-  text-align: center;
-  color: #999;
-  margin: 80rpx 0
-}
+	.home-menu {
+		display: flex;
+		justify-content: space-around;
+		align-items: center;
+		margin: 20rpx 0;
+		height: 180rpx;
+		border-radius: 5rpx;
+		background-color: #FFFFFF;
 
-.t-login .t-e {
-  text-align: center;
-  width: 250rpx;
-  margin: 80rpx auto 0
-}
+		.row {
+			width: 30%;
+			font-size: 28rpx;
+			font-weight: bold;
+			flex-direction: column;
 
-.t-login .t-g {
-  /*float: left;*/
-  /*width: 100%*/
-}
+			view {
+				line-height: 50rpx;
+			}
+		}
 
-.t-login .t-e image {
-  width: 60rpx;
-  height: 60rpx
-}
+		.edge {
+			border-left: 5rpx solid #f1f1f1;
+			border-right: 5rpx solid #f1f1f1;
+		}
+	}
 
-.t-login .t-f {
-  text-align: center;
-  margin: 80rpx 0 0 0;
-  color: #666
-}
+	.list-menu {
+		background-color: #FFFFFF;
 
-.t-login .t-f text {
-  margin-left: 8rpx;
-  color: #999
-}
+		.cell {
+			display: flex;
+			justify-content: space-between;
+			align-items: center;
+			height: 100rpx;
+			border-top: 5rpx solid #f8f8f8;
 
-.t-login .uni-input-placeholder {
-  color: #000
-}
+			.left-icon {
+				width: 15%;
 
-.cl {
-  zoom: 1
-}
+				image {
+					width: 50rpx;
+					height: 50rpx;
+				}
+			}
 
-.cl:after {
-  clear: both;
-  display: block;
-  visibility: hidden;
-  height: 0;
-  content: '\20'
-}
+			.txt {
+				width: 75%;
+				font-size: 26rpx;
+			}
+
+			.right-icon {
+				width: 10%;
+
+				image {
+					width: 30rpx;
+					height: 30rpx;
+				}
+			}
+		}
+	}
+	.logout {
+		margin-top: 30px;
+	}
 </style>
