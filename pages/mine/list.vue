fb6f97bc1c820d7445abea97f6a8e31f6d1bb489
zhenhuaixiu <huaixiu.zhen@gmail.com>
2021-04-24 20:42:55 +0800

    my timeline list

--- pages/mine/list.vue
+++ pages/mine/list.vue
@@ -1,21 +1,26 @@
 <template>
 	<view class="container" :style="{opacity:pageOpacity}">
-	<view class="like-wrap">
-		<template v-if="cardList && cardList.length > 0">
-		<view class="margin-bottom" v-for="(item, index) in cardList" :key="index">
-			<y-DiaryItem :obj="item" />
+		<view class="like-wrap">
+			<template v-if="cardList && cardList.length > 0">
+				<view class="margin-bottom" v-for="(item, index) in cardList" :key="index">
+					<y-DiaryItem :obj="item" />
+				</view>
+				<y-LoadMore :status="loadMoreStatus" />
+			</template>
+			<template v-else>
+				<y-Empty />
+			</template>
 		</view>
-		</template>
-		<template v-else>
-			<y-Empty />
-		</template>
-	</view>
 	</view>
 </template>
 
 <script>
+	import { getUserTimelines } from '@/apis/timelines.js'
+	import { getMyFollowPostsList } from '@/apis/action.js'
 	import { mapState, mapActions } from 'vuex'
 	import yEmpty from "../article/components/y-Empty/y-Empty";
+	import yLoadMore from "../article/components/y-LoadMore/y-LoadMore";
+	import yDiaryItem from "../article/components/y-DiaryItem/y-DiaryItem";
 
 	var that;
 	export default {
@@ -26,13 +31,19 @@
 				startNum: 1,
 				type: '',
 				uuid: '',
+
+				pageSize: 10,
+				currentPage: 1,
+				loadMoreStatus: 0,
 			}
 		},
 		computed: {
 			...mapState(['hasBinding', 'hasLogin']),
 		},
 		components:{
-			yEmpty
+			yDiaryItem,
+			yEmpty,
+			yLoadMore
 		},
 		onLoad(e) {
 			// 在需要登录的地方执行初始化方法
@@ -52,13 +63,13 @@
 					uni.setNavigationBarTitle({
 						title: '我的收藏'
 					})
-					this.getMyCollect()
+					this.getMyCollect(this.currentPage)
 
 				} else {
 					uni.setNavigationBarTitle({
 						title: '我的日记'
 					})
-					this.getMyTimeline()
+					this.getMyTimeline(this.currentPage)
 				}
 			}
 		},
@@ -68,12 +79,43 @@
 		methods: {
 			...mapActions(['initLoginState']),
 
-			getMyCollect() {
+			getMyCollect(page) {
+				if (this.loadMoreStatus === 2) {
+					return false
+				}
+				this.loadMoreStatus = 1
 
+				let that = this
+				// getMyFollowPostsList(this.uuid, page).then(res => {
+				// 	that.loadMoreStatus = res.data.length < this.pageSize ? 2: 0
+				// 	that.cardList = that.cardList.concat(res.data)
+				// })
 			},
-			getMyTimeline() {
-				
-			}
+			getMyTimeline(page) {
+				if (this.loadMoreStatus === 2) {
+					return false
+				}
+				this.loadMoreStatus = 1
+
+				let that = this
+				getUserTimelines(this.uuid, page).then(res => {
+					that.loadMoreStatus = res.data.data.length < this.pageSize ? 2: 0
+					that.cardList = that.cardList.concat(res.data.data)
+				})
+			},
+			onReachBottom() {
+				if (this.type === 'collect') {
+					this.currentPage = this.currentPage + 1
+					setTimeout(() => {
+						this.getMyCollect(this.currentPage);
+					}, 1000);
+				} else {
+					this.currentPage = this.currentPage + 1
+					setTimeout(() => {
+						this.getMyTimeline(this.currentPage);
+					}, 1000);
+				}
+			},
 		}
 	}
 </script>
