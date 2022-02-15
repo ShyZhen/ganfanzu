7daa5af07cbad609304c8df82ae111391bdff972
zhenhuaixiu <zhenhuaixiu@sznews.com>
2022-02-15 15:24:24 +0800

    优化首页登录逻辑

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -150,6 +150,7 @@
 import { getShareObj } from "@/utils/share.js"
 import { getCouponsNew } from "@/apis/ganfan.js"
 import { mapState, mapActions } from 'vuex'
+import { wxmpLogin } from '@/utils/loginPlugin.js'
 
 export default {
   data() {
@@ -234,24 +235,45 @@ export default {
         url: '/pages/index/link'
       })
     },
+    jumpAction(info) {
+      wx.navigateToMiniProgram({
+        appId: info.minapp.appid,
+        path: info.minapp.path,
+        success: (res) => {
+          console.log(res)
+        }
+      })
+    },
     goNextPage(info) {
       if (this.hasLogin) {
-        wx.navigateToMiniProgram({
-          appId: info.minapp.appid,
-          path: info.minapp.path,
-          success: (res) => {
-            console.log(res);
-          }
-        })
+        this.jumpAction(info)
       } else {
-        this.$toast('领取需要登录')
-        setTimeout(() => {
-          this.saveLoading = false
-          this.$toLogin()
-        }, 1000);
-        return false
+        this.getUserProfile(info)
       }
     },
+    getUserProfile(info) {
+      // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认
+      // 开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
+      let that = this
+      uni.getUserProfile({
+        desc: '用于完善个人资料', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+        success: (res) => {
+          that.$loading('拼命加载中...')
+          if (res.userInfo) {
+            let platform = 'weixin'
+            wxmpLogin(res.userInfo, platform).then(res => {
+              that.$loading(false)
+              that.jumpAction(info)
+            }).catch(err => {
+              setTimeout(() => {this.$toast('微信登录失败'), 500})
+            })
+          } else {
+            that.$toast('获取用户信息失败')
+          }
+        },
+      })
+    },
+
     goAction() {
 
     },
